b7e51e5785bf5de922f27c4f405010ffa56a6465
RachelEJ <rej3713@gmail.com>
2021-03-13 16:07:30 -0600

    Update anon_login.php

--- website/html/anon_login.php
+++ website/html/anon_login.php
@@ -2,6 +2,6 @@
 	$hn = '127.0.0.1';
 	$un = 'anon';
 	$pw = ''; //To be completed if you have set a password to root
-	$db = 'beanz_test'; //To be completed to connect to a database. The database must exist.
+	$db = 'beanz'; //To be completed to connect to a database. The database must exist.
 	$port = 3307; //Default must be NULL to use default port
 ?>
