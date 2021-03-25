92337f8780f60c2e50187d83a30f2d6daf530db0
RachelEJ <rej3713@gmail.com>
2021-03-24 20:21:51 -0500

    Delete website/html directory
    Trying to redo these files with the merged code

--- website/html/clear_session.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	session_start();
-	$_SESSION = array();
-	header("Location: home.php");
-
-?>
--- website/html/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$hn = '127.0.0.1';
-	$un = 'root';
-	$pw = '';  // phpMyAdmin password
-	$db = 'beanz';  // database name, should be netID
-	$port = 3307;
-?>
