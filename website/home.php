cbebbaa23c2a1f3269fd422f1d847bca2d29de8c
RachelEJ <rej3713@gmail.com>
2021-03-26 13:56:32 -0500

    Update home.php
    Changed IDs and classes to optimize HTML and CSS code. Also added a View Profile button and an anchor tag around the like count. View Profile should link to the user's profile. Anchor tag should link to the likesList page.

--- website/home.php
+++ website/home.php
@@ -16,21 +16,20 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en-us">
 <head> <!-- Meta Data -->
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="main.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
-    <title>BeanzCroc</title>
-    <!-- add links to fonts here -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="main.css">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
+  <title>BeanzCroc</title>
 </head>
 <body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script>
-        
-        // Tell this code to only run once the page is fully loaded.
-        $(document).ready(function(){
+  <script>
+    // Tell this code to only run once the page is fully loaded.
+    $(document).ready(function(){
             var likesCount = 8;
             var tid = 0;
             var postHandle = "";
@@ -87,7 +86,7 @@
                 if (loggedInUser == ""){
                     alert("Please log in to follow a user.");
                 } else if (postHandle == "Handle Here") {
-                    alert("Can't follow a non-existant user. Try clicking a post before attempting to follow a user.");
+                    alert("Can't follow a non-existent user. Try clicking a post before attempting to follow a user.");
                 } else {
                     xhttp.open("GET", "follow_user.php", false);
                     xhttp.send("handle_follower="+loggedInUser+"&"+"handle_following="+postHandle); // NOTE: ANYONE CAN MAKE ANY USER FOLLOW ANY OTHER USER. WE SHOULD REQUIRE A PASSWORD OR SOMETHING.
@@ -107,117 +106,110 @@
                 // // Simulate a mouse click:
                 // window.location.href = "follow.php?handle="+postHandle;
             });
-
-            
-
-
         });
-    </script>
-    <header> <!-- Header bar at top -->
-        <a href="home.php"> <!-- "Logo" and link back to the home page -->
-            <h1>BeanzCroc</h1>
-        </a>
-        <nav> <!-- Navigation buttons -->
-            <ul>
-                <li class="navButtons">
-                    <a href="settings.html">Settings</a>
-                </li>
+  </script>
 
-                <?php 
-                    if ($logged_in == False){
-                        echo "<li class='navButtons'>
-                        <a href='registerAccount.php'>Register</a></li>";
+  <header> <!-- Header bar at top -->
+    <a href="home.php"> <!-- "Logo" and link back to the home page -->
+      <h1>BeanzCroc</h1>
+    </a>
+    <nav> <!-- Navigation buttons -->
+      <ul>
+        <li class="navButtons">
+          <a href="settings.html">Settings</a>
+        </li>
 
-                        echo "<li class='navButtons'>
-                        <a href='loginPage.php'>Login</a></li>";
-                    } else {
-                        echo "<li class='navButtons'>
-                        <a href='userProfile.html'>".$_SESSION["handle"]."</a></li>";
+        <?php 
+            if ($logged_in == False){
+                echo "<li class='navButtons'>
+                    <a href='registerAccount.php'>Register</a></li>";
 
-                        echo "<li class='navButtons'>
-                        <a href='clear_session.php'>Logout</a></li>";
-                    }
-                    
-                ?>
+                echo "<li class='navButtons'>
+                    <a href='loginPage.php'>Login</a></li>";
+            } else {
+                echo "<li class='navButtons'>
+                    <a href='userProfile.html'>".$_SESSION["handle"]."</a></li>";
 
-                <li class="navButtons">
-                    <a href="home.php">Home</a>
-                </li>
-            </ul> 
-        </nav>
-    </header>
-    <!-- Starts the body for the home page -->
+                echo "<li class='navButtons'>
+                    <a href='clear_session.php'>Logout</a></li>";
+            }             
+        ?>
 
-    <!-- Post a Beanz / Feed -->
-    <div id="leftCol" class="col">
-        <a href="postABeanz.php" id="postBeanz">
-            Post a Beanz
-        </a>
-        <div id="feed">
-            <div id="feedTitle">
-                <h3>Feed</h3>
-            </div>
-            <?php
-                // Add this before trying to access database contents
-                require_once 'login.php';
-                $conn = new mysqli($hn, $un, $pw, $db, $port);
+        <li class="navButtons">
+          <a href="home.php">Home</a>
+        </li>
+      </ul> 
+    </nav>
+  </header>
+  <!-- Starts the body for the home page -->
 
-                if ($conn->connect_error) {
-                    die('Connect Error (' . $conn->connect_errno . ') '
-                            . $conn->connect_error);
+  <!-- Post a Beanz / Feed -->
+  <div id="leftCol" class="col">
+    <a href="postABeanz.php" id="postBeanz"> Post a Beanz </a>
+
+      <div>
+        <h3 id="feedTitle">Feed</h3>
+      </div>
+
+      <div class="feed">
+        <?php
+            // Add this before trying to access database contents
+            require_once 'login.php';
+            $conn = new mysqli($hn, $un, $pw, $db, $port);
+
+            if ($conn->connect_error) {
+                die('Connect Error (' . $conn->connect_errno . ') '
+                      . $conn->connect_error);
+            }
+
+            // echo 'Connection OK '. $conn->host_info.'<br>';
+            // echo 'Server '.$conn->server_info.'<br>';
+            // echo 'Initial charset: '.$conn->character_set_name().'<br>';
+
+
+            $result = $conn->query("SELECT * from tweets");
+
+
+
+            if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()){
+                    echo "<p id='".$row["tid"]
+                    ."' onclick=\"display_tweet("
+                    .$row["tid"]. ", '" .$row["tweet_title"]. "','" .$row["content"]. "','" . $row["handle"]. "','" .$row["handle"] ."')\">"     // DATA FROM HERE  "','".$row["total_likes"].
+                    . $row["tweet_title"] 
+                    . "</p>";
+                    echo "<script></script>";
                 }
+            } else { echo "No tweets found in table."; }
 
-                // echo 'Connection OK '. $conn->host_info.'<br>';
-                // echo 'Server '.$conn->server_info.'<br>';
-                // echo 'Initial charset: '.$conn->character_set_name().'<br>';
-
-
-                $result = $conn->query("SELECT * from tweets");
-
-
-
-                if($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()){
-                        echo "<p id='".$row["tid"]
-                        ."' onclick=\"display_tweet("
-                        .$row["tid"]. ", '" .$row["tweet_title"]. "','" .$row["content"]. "','" . $row["handle"]. "','" .$row["handle"] ."')\">"     // DATA FROM HERE  "','".$row["total_likes"].
-                        . $row["tweet_title"] 
-                        . "</p>";
-                        echo "<script></script>";
-                    }
-                } else { echo "No tweets found in table."; }
-
-                $conn->close(); // Close the connection when we're done
-            ?>
+            $conn->close(); // Close the connection when we're done    
+        ?>
             
-        </div>
-    </div>
-    
+      </div>
+  </div>
+  
+  <!-- Beanz full message -->
+  <div id="fullBeanz" class="col">
+    <p id="tweetID" hidden>tid_here</p>
+    <p class="beanzTitle" id="mainBTitle">The Beanz Title will appear here!</p>
+    <p class="beanzText" id="mainBText">Please select a Beanz on the left. The Beanz Text will appear here!</p>
+    <button id="likeButton">Like this Beanz</button>
+    <a href="likesList.html">
+      <p id="likeCount">0</p>
+    </a>
+  </div>
 
+  <!-- User info -->
+  <div id="userInfo" class="col">
+    <p class="colUser" id="mainUserName">Username Here</p>
+    <p class="colHandle" id="mainUserHandle">Handle Here</p>
+    <button id="ViewProfile">View User Profile</button>
+    <button id="ViewFollowButton">View Follows</button>
+    <button id="FollowButton">Follow this User</button>
+  </div>
 
-    <!-- Beanz full message -->
-    <div id="fullBeanz" class="col">
-        <p id="tweetID" hidden>tid_here</p>
-        <p id="mainBTitle">The Beanz Title will appear here!</p>
-        <p id="mainBText">Please select a Beanz on the left. The Beanz Text will appear here!</p>
-        <button id="likeButton">Like this Beanz</button>
-        <p id="likeCount">0</p>
-    </div>
-
-    <!-- User info -->
-    <div id="userInfo" class="col">
-        <p id="mainUserName">Username Here</p>
-        <p id="mainUserHandle">Handle Here</p>
-        <!-- <a href="userProfile.html">
-            <button id="ViewProfile">View User Profile</button>
-        </a> -->
-        <button id="ViewFollowButton">View Follows</button>
-        <button id="FollowButton">Follow this User</button>
-    </div>
- 
 </body>
 </html>
-
 
 
 <script>
