d6b97a3d8793ac52a06da9002b335c68ccac4ef3
RachelEJ <rej3713@gmail.com>
2021-03-26 14:19:34 -0500

    Update postABeanz.php
    Uploaded the wrong one last time. This one updates the IDs and classes.

--- website/postABeanz.php
+++ website/postABeanz.php
@@ -11,13 +11,14 @@
     } else {
         // echo "Username is not set.";
         $logged_in = False;
+        header("Location: loginPage.php");
     }
 
 ?>
 
 <!DOCTYPE html>
-<html>
-<head lang="en-us"> <!-- Meta Data -->
+<html lang="en-us">
+<head> <!-- Meta Data -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="main.css">
@@ -33,93 +34,56 @@
         <li class="navButtons">
           <a href="settings.html">Settings</a>
         </li>
-                
+
         <?php 
-          if ($logged_in == False){
-            echo "<li class='navButtons'>
-            <a href='registerAccount.php'>Register</a></li>";
+            if ($logged_in == False){
+                echo "<li class='navButtons'>
+                <a href='registerAccount.php'>Register</a></li>";
 
-            echo "<li class='navButtons'>
-            <a href='loginPage.php'>Login</a></li>";
-          } else {
-            echo "<li class='navButtons'>
-            <a href='userProfile.html'>".$_SESSION["handle"]."</a></li>";
+                echo "<li class='navButtons'>
+                <a href='loginPage.php'>Login</a></li>";
+            } else {
+                echo "<li class='navButtons'>
+                <a href='userProfile.html'>".$_SESSION["handle"]."</a></li>";
 
-            echo "<li class='navButtons'>
-            <a href='clear_session.php'>Logout</a></li>";
-        }         
-                    
+                echo "<li class='navButtons'>
+                <a href='clear_session.php'>Logout</a></li>";
+            }          
         ?>
 
         <li class="navButtons">
-          <a href="home.php">Home</a>
+           <a href="home.php">Home</a>
         </li>
       </ul> 
     </nav>
   </header>
+
+  <!-- Starts the body for the Post a Beanz page -->
+  <h4 id="beanzBigTitle">Post a Beanz</h4>
   
-  <!-- Starts the body for the follow page -->
-  <div>
-        
-    <!-- The User's Handle and the Follower/Following titles at the top of the page-->
-    <div> 
-      <h4 id="beanzBigTitle"><?php echo $_GET["handle"]; ?></h4>
-      <h3 class="colTitle colLeft">Followers</h3>
-      <h3 class="colTitle colRight">Following</h3>
-    </div>
-        
-    <!-- The Follower/Following columns div -->
-    <div>
-      <!-- Followers Column -->
-      <div class="folCol nameListCol colLeft"> <!-- People who follow this user -->
+  <div> <!-- The form to take input from user for Beanz -->
+    <form action="recieve_tweet.php" method="POST" class="beanzForm">
+      <!-- Title for the Beanz -->
+      <label for="bTitle">Beanz Title:</label><br>
+      <input name="bTitle" placeholder="Enter Beanz Title" type="text" size="30" minlength="1" maxlength="50" required><br>
 
-      <!-- The folEntry divs will most likely need to be recreated in PHP in a while loop so that all followers/following users are displayed. -->
-      <!-- Comment them out and then create them in PHP with the same general format. The Profile button can be ignored for now. -->
-      <!-- Make sure that all IDs and classes carry over when replicating them in PHP -->
-
-        
-        <?php 
-            require_once "database.php";
-            $database = new Database();
-            $result = $database->get_followers($_GET["handle"]);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<div class='nameListEntry'>
-                    <p class='nameListP'>".$row["follower_handle"]."</p>
-                    </div>";
-                }
-            } else { echo "No followers"; }
-        ?>
-
-        <!-- 1 entry in the Follower column -->
-        <!-- <div class="nameListEntry">
-          <p class="nameListP">FollowerName</p>
-          <button id="nameListProfile" class="folButton">Profile</button> we don't need this just yet
-        </div> -->
-      </div>
-        
-      <!-- Following Column -->
-      <div class="folCol nameListCol colRight"> <!-- People whom this user follows-->
-        <!-- 1 entry in the Following column -->
-        <!-- <div class="nameListEntry">
-          <p class="nameListP">FollowingName</p>
-          <button id="nameListProfile" class="folButton">Profile</button> we don't need this just yet
-        </div> -->
-
-        <?php 
-            require_once "database.php";
-            $result = $database->get_following($_GET["handle"]);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<div class='nameListEntry'>
-                        <p class='nameListP'>".$row["following_handle"]."</p>
-                        </div>";
-                }
-            } else { echo "Not following anyone"; }
-        ?>
-
-      </div>
-    </div>
+      <!-- Actual text for the Beanz -->
+      <label for="bText">Beanz Text:</label><br>
+      <textarea name="bText" placeholder="Enter Beanz Test" rows="5" cols="26" minlength="1" maxlength="140" required></textarea><br>
+            
+      <!-- Submit and Clear buttons -->
+      <input name="bSubmit" type="submit" class="formButton" id="beanzSubmit" value=
+      <?php 
+          if (isset($_SESSION['handle'])) {
+              // echo "'Submit' name='submit'";
+              echo "'".$_SESSION['handle']."'";
+          } else {
+              echo "'Sign in to submit' disabled";
+          }          
+      ?> >
+      <input type="reset" class="formButton" id="beanzClear" value="Clear">
+    </form>
   </div>
+    
 </body>
 </html>
