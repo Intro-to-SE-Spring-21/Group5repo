81541e8bcfb291bb966bfaefa7b3855f1c75e0bb
Lundy <77692819+Ydnul@users.noreply.github.com>
2021-04-03 21:29:25 -0500

    Update TweetTest.php

--- tests/Unit/TweetTest.php
+++ tests/Unit/TweetTest.php
@@ -17,7 +17,7 @@ class UserTest extends TestCase
 		$tweet->date_posted = "4/2/2021";
 		$tweet->total_likes = 100;
 
-		$this->assertEquals($user->getTweet(), "Tid: 1 Handle: Thomas Tweet_title: Hello World Content: Hello my name is Thomas Date_posted: 4/2/2021 Total_likes: 100");
+		$this->assertEquals($tweet->getTweet(), "Tid: 1 Handle: Thomas Tweet_title: Hello World Content: Hello my name is Thomas Date_posted: 4/2/2021 Total_likes: 100");
 	}
 
 }
